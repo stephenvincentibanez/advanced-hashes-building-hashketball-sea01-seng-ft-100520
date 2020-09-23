c7103aa268be2a9573f21b1a1e4e40177c4f54a3
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:46:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,6 +90,16 @@ def game_hash
       steals: 4, 
       blocks: 5,
       slam_dunks: 5},
+      
+      {player_name: "Ben Gordon",
+      number: 8, 
+      shoe: 15,
+      points: 33, 
+      rebounds: 3, 
+      assists: 7, 
+      steals: 22, 
+      blocks: 15,
+      slam_dunks: 10},
       ]
   }
 }
