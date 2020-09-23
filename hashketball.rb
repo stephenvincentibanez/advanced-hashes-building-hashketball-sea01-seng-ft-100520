41041a576e10986c4899f9f450ca384e0064eed5
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 21:58:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash (home, away)
 home = {
   team_name: "Brooklyn Nets"
   colors: ["Black", "White"]
-  players: {
+  players: [
     {player_name: "Alan Anderson"},
     {number: "0"},
     {shoe:"16"},
@@ -16,8 +16,6 @@ home = {
     {blocks: "1"},
     {slam_dunks: "1"}
     ]
-  }
-}
 binding.pry
 away = {}
 end
