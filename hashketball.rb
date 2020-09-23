5dd71c8bd5d2082a03198b91e53886660019248c
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:33:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,17 @@ def game_hash
   home: {
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
-    players: [{}]
+    players: [{
+      player_name: "Alan Anderson",
+      number: "0",
+      shoe: "16",
+      points: "22",
+      rebounds: "12", 
+      assists "12",
+      steals: "3",
+      blocks: "1",
+      slam_dunks "1"
+    }]
   },
   away: {
     team_name: "Charlotte Hornets", 
