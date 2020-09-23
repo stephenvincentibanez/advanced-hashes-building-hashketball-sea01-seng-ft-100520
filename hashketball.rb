3d2f4c767c6fbc0ee902a08e3ea86f5193b9a6f2
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:46:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -96,10 +96,10 @@ def game_hash
       shoe: 15,
       points: 33, 
       rebounds: 3, 
-      assists: 7, 
-      steals: 22, 
-      blocks: 15,
-      slam_dunks: 10},
+      assists: 2, 
+      steals: 1, 
+      blocks: 1,
+      slam_dunks: 0},
       ]
   }
 }
