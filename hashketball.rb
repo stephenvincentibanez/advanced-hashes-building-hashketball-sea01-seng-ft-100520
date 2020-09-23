56523eed54df64af4e679441f78cd9c613bc5fdd
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:30:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,11 +4,13 @@ def game_hash
 {
   home: {
     team_name: "Brooklyn Nets",
-    colors: ["Black", "White"]
+    colors: ["Black", "White"],
+    players: []
   },
   away: {
     team_name: "Charlotte Hornets", 
-    colors: ["Turquoise", "Purple"]
+    colors: ["Turquoise", "Purple"],
+    players: []
   }
 }
 end
