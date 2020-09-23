3a87ceeee23f43db86b591badf0100385032f235
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:41:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -44,7 +44,17 @@ def game_hash
       assists: 6,
       steals: 3,
       blocks: 8,
-      slam_dunks: 5}
+      slam_dunks: 5},
+      
+      {player_name: "Jason Terry",
+      number: 31,
+      shoe: 15,
+      points: 19,
+      rebounds: 2, 
+      assists: 2,
+      steals: 4,
+      blocks: 11,
+      slam_dunks: 1}
     ]
   },
   away: {
