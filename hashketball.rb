95372340aef104b0dbf7e506b927a2a28ecac56d
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 22:45:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,6 +80,16 @@ def game_hash
       steals: 22, 
       blocks: 15,
       slam_dunks: 10},
+      
+      {player_name: "DeSagna Diop",
+      number: 2, 
+      shoe: 14,
+      points: 24, 
+      rebounds: 12, 
+      assists: 12, 
+      steals: 4, 
+      blocks: 5,
+      slam_dunks: 5},
       ]
   }
 }
